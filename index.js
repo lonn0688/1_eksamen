09af1a3ff4c00e1c363474ff9d29a4b800e37282
Lonni Sandbjerg <lonnisandbjerg@Lonnis-MBP.lan>
2018-06-04 20:57:58 +0200

    mandag aften

---
+++ index.js
@@ -0,0 +1,13 @@
+$(window).on("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+
+
+
+
+    $("#bunny_container").addClass("bunny_container_walk_in");
+    $("#bunny_container").addClass("bunny_start_position");
+    $("#bunny_sprite").addClass("bunny_paws");
+   // $("#bunny_container").on("animationend", trin1A);
